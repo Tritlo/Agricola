2901ec79fee90abf5791927a7885f4ea07c5963e
solrun <solalola@gmail.com>
2015-12-14 19:28:50 +0100

    animals in buildings

--- src/Update.hs
+++ src/Update.hs
@@ -435,9 +435,16 @@ hasBorder farm (cn,cm) W = farm ^. (border V cn cm . isThere)
 hasBorder farm (cn,cm) E = farm ^. (border V cn (cm + 1) . isThere)
 
 animalCapacity :: Agricola -> Coord -> Integer
-animalCapacity agri c | not (isEnclosed agri c) = hasTrough agri c
-                      | isEnclosed agri c = 2 ^ (1 + troughNum agri c)
+animalCapacity agri c@(cx,cy) =
+  case (agri ^. player col . farm . tile cx cy . building) of
+  Just FarmHouse -> 1*(2^t)
+  Just Stall -> 3*(2^t)
+  Just Stable -> 5*(2^t)
+  Nothing -> if (isEnclosed agri c)
+             then  2 ^ (1 + troughNum agri c)
+             else t
   where col = agri ^. whoseTurn
+        t   = hasTrough agri c
 
 animalSpace :: Agricola -> Coord -> Integer
 animalSpace agri c@(cx,cy) | isNothing tileAn = animalCapacity agri c
